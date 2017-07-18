648ed66975e5259424bbdbb4332df5492d97c012
Daniel Martin <etd-github@nomejortu.com>
2017-07-18 17:15:45 +0200

    Thor - use Framework's logger

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -10,9 +10,6 @@ class NmapTasks < Thor
   def upload(file_path)
     require 'config/environment'
 
-    logger = Logger.new(STDOUT)
-    logger.level = Logger::DEBUG
-
     unless File.exists?(file_path)
       $stderr.puts "** the file [#{file_path}] does not exist"
       exit(-1)
@@ -22,7 +19,5 @@ class NmapTasks < Thor
 
     importer = Dradis::Plugins::Nmap::Importer.new(logger: logger)
     importer.import(file: file_path)
-
-    logger.close
   end
 end
