2a073e690e47064bf302c22880cea26eaae7c3bd
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-01-29 23:20:58 +0100

    Improve syntax to please Rails 6's loader

--- lib/dradis/plugins/nmap/engine.rb
+++ lib/dradis/plugins/nmap/engine.rb
@@ -1,13 +1,9 @@
-module Dradis
-  module Plugins
-    module Nmap
-      class Engine < ::Rails::Engine
-        isolate_namespace Dradis::Plugins::Nmap
+module Dradis::Plugins::Nmap
+  class Engine < ::Rails::Engine
+    isolate_namespace Dradis::Plugins::Nmap
 
-        include ::Dradis::Plugins::Base
-        description 'Processes Nmap output'
-        provides :upload
-      end
-    end
+    include ::Dradis::Plugins::Base
+    description 'Processes Nmap output'
+    provides :upload
   end
 end
