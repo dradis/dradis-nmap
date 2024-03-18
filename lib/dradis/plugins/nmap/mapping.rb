d2e278c08d434dfbf47b7785b1736894c18c6129
Matt Budz <matt.budz@protonmail.com>
2024-03-18 19:30:42 +0100

    use constant for default mappings

--- lib/dradis/plugins/nmap/mapping.rb
+++ lib/dradis/plugins/nmap/mapping.rb
@@ -1,24 +1,22 @@
 module Dradis::Plugins::Nmap
   module Mapping
-    def self.default_mapping
-      {
-        'host' => {
-          'Title' => 'Nmap info: {{ nmap[host.ip] }}',
-          'IP' => '{{ nmap[host.ip] }}',
-          'Hostnames' => '{{ nmap[host.hostnames] }}',
-          'OS' => '{{ nmap[host.os] }}',
-          'Services' => "|_. Port number |_. Protocol |_. State |_. Service |_. Product |_. Version |\n
-                        {{ nmap[host.service_table] }}",
-          'Type' => 'Properties'
-        },
-        'port' => {
-          'Title' => '{{ nmap[port.number] }}/{{ nmap[port.protocol] }} is {{ nmap[port.state] }} ({{ nmap[port.reason] }})',
-          'Service' => '{{ nmap[port.service.name] }}',
-          'Product' => '{{ nmap[port.service.product] }}',
-          'Version' => '{{ nmap[port.service.version] }}',
-          'Host' => '{{ nmap[port.host] }}'
-        }
+    DEFAULT_MAPPING = {
+      host: {
+        'Title' => 'Nmap info: {{ nmap[host.ip] }}',
+        'IP' => '{{ nmap[host.ip] }}',
+        'Hostnames' => '{{ nmap[host.hostnames] }}',
+        'OS' => '{{ nmap[host.os] }}',
+        'Services' => "|_. Port number |_. Protocol |_. State |_. Service |_. Product |_. Version |\n
+                      {{ nmap[host.service_table] }}",
+        'Type' => 'Properties'
+      },
+      port: {
+        'Title' => '{{ nmap[port.number] }}/{{ nmap[port.protocol] }} is {{ nmap[port.state] }} ({{ nmap[port.reason] }})',
+        'Service' => '{{ nmap[port.service.name] }}',
+        'Product' => '{{ nmap[port.service.product] }}',
+        'Version' => '{{ nmap[port.service.version] }}',
+        'Host' => '{{ nmap[port.host] }}'
       }
-    end
+    }.freeze
   end
 end
